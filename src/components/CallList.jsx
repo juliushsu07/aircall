06613b91db5df9f973c858062ca2cf2d957aa73c
Julius Hsu <32544328+juliushsu07@users.noreply.github.com>
2022-12-20 18:23:49 -0500

    Create dummy data json, Update Call List to render caller Data dynamically, Import Icons.

--- src/components/CallList.jsx
+++ src/components/CallList.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react';
 import CallListItem from './CallListItem.jsx';
+import callerData from "../data/calls.json";
+import {HiOutlineArchive} from 'react-icons/hi';
 
 function CallList() {
+  const [callers] = useState(callerData);
+
   return (
-    <div>
-        <CallListItem></CallListItem>
-        <CallListItem></CallListItem>
-        <CallListItem></CallListItem>
-        <CallListItem></CallListItem>
-        <CallListItem></CallListItem>
+      <div>
+        <button type="button" className="callerButton">
+          <HiOutlineArchive className="caller-icon" />
+          Archive all calls
+        </button>
+
+        <ul>
+        {callers.map((caller) => (
+          <CallListItem
+          key={caller.id}
+          from={caller.from}
+          time={caller.time}
+          date={caller.date}
+          via={caller.via}
+          call_type={caller.call_type}
+          is_archived={caller.is_archived}
+          />
+        ))}
+      </ul>
     </div>
   )
 }
--- src/components/CallListItem.jsx
+++ src/components/CallListItem.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {FcMissedCall} from 'react-icons/fc';
 
-function CallListItem() {
+function CallListItem({ from, time, date, via, call_type, is_archived  }) {
   return (
-    <div>Item</div>
+    <div className="callerWrapper">
+
+      <div className='date'>
+        {date}
+      </div>
+
+      <button type="button" className="callerButton">
+      <FcMissedCall className="caller-icon" />
+        <p>{from}</p>
+        <p>tried to call on {via}</p>
+        <p>{time}</p>
+      </button>
+
+    </div>
   )
 }
 
