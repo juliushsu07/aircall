dfb04ba26e173f5972f679af8a5385cfbd56aea2
Julius Hsu <32544328+juliushsu07@users.noreply.github.com>
2022-12-21 04:00:39 -0500

    Rename CallListItem to MissedCalls. Fetch data from API call.

--- src/components/CallList.jsx
+++ src/components/CallList.jsx
@@ -1,35 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import CallListItem from './CallListItem.jsx';
-import {HiOutlineArchive} from 'react-icons/hi';
-import useFilterCalls from '../hooks/useFilterCalls.js';
-import calls from "../data/calls.json";
+import React, { useState, useEffect } from "react";
+import { HiOutlineArchive } from "react-icons/hi";
+import useFilterCalls from "../hooks/useFilterCalls.js";
+import MissedCalls from "./MissedCalls.jsx";
+import axios from "axios";
 
 function CallList() {
-  const {all, missedInboundUnarchived} = useFilterCalls(calls);
+  const [calls, setCalls] = useState();
+
+  const { all, missedInboundUnarchived } = useFilterCalls(calls);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://charming-bat-singlet.cyclic.app/https://cerulean-marlin-wig.cyclic.app/activities"
+      )
+      .then((res) => {
+        const calls = [...res.data];
+        setCalls(calls);
+      });
+  }, []);
 
   return (
-      <div>
-        <button type="button" className="callerButton">
-          <HiOutlineArchive className="caller-icon" />
-          Archive all calls
-        </button>
+    <div>
+      <button type="button" className="callerButton">
+        <HiOutlineArchive className="caller-icon" />
+        Archive all calls
+      </button>
 
-        <ul>
-        {missedInboundUnarchived(calls).map((call) => (
-          <CallListItem
-          key={call.id}
-          direction={call.direction}
-          from={call.from}
-          via={call.via}
-          duration={call.duration}
-          call_type={call.call_type}
-          is_archived={call.is_archived}
-          created_at={call.created_at}
-          />
-        ))}
+      <ul>
+        {calls &&
+          missedInboundUnarchived(calls).map((call) => (
+            <MissedCalls
+              key={call.id}
+              direction={call.direction}
+              from={call.from}
+              via={call.via}
+              duration={call.duration}
+              call_type={call.call_type}
+              is_archived={call.is_archived}
+              created_at={call.created_at}
+            />
+          ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default CallList
+export default CallList;
--- src/hooks/useFilterCalls.js
+++ src/hooks/useFilterCalls.js
@@ -1,16 +1,19 @@
 function useFilterCalls(calls) {
+  const all = (calls) => {
+    return calls;
+  };
 
-      const all = (calls) => {
-        return calls
-      };
+  const missedInboundUnarchived = (calls) => {
+    return calls.filter((call) => {
+      return (
+        call.call_type == "missed" &&
+        call.direction == "inbound" &&
+        !call.is_archived
+      );
+    });
+  };
 
-      const missedInboundUnarchived = (calls) => {
-        return calls.filter( (call) => {
-          return call.call_type == 'missed' && call.direction == 'inbound' && !call.is_archived ;
-        })
-      };
-    
-    return {all, missedInboundUnarchived}
+  return { all, missedInboundUnarchived };
 }
 
-export default useFilterCalls
+export default useFilterCalls;
